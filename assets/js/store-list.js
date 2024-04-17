a7fc56440047fdb19fda8cec3484bb26b23406e7
Mr-Bellali <bellali.yassine52@gmail.com>
2024-04-17 23:11:24 +0100

    implementation de filtrage et de changement de display

--- assets/js/store-list.js
+++ assets/js/store-list.js
@@ -1,7 +1,10 @@
 let storeLocations = [];
+
 if (localStorage.getItem('storeStorage')) {
     const storeStorage = JSON.parse(localStorage.getItem('storeStorage'));
     storeLocations = storeStorage.map(store => store.storeLocation);
+    // Remove duplicates
+    storeLocations = Array.from(new Set(storeLocations));
 }
 
 const storeLocationSelect = document.getElementById('locationSelect');
@@ -12,42 +15,76 @@ storeLocations.forEach(storeLocation => {
     storeLocationSelect.appendChild(option);
 });
 
-
-//function to change the id of the stores display
-
-
-function ChangeID(storeId, newStoreId) {
-    var storeDisplay = document.getElementById(storeId);
-    storeDisplay.id = newStoreId;
-}
-
-
-
-
-document.addEventListener("DOMContentLoaded", function () {
-
-    renderData();
+storeLocationSelect.addEventListener('change', function () {
+    const selectedLocation = this.value;
+    renderData(selectedLocation);
 });
 
-// function to get name and city of the store
-function renderData() {
+document.addEventListener("DOMContentLoaded", function () {
+    renderData("all");
+});
+
+function renderData(selectedLocation) {
     const stores = JSON.parse(localStorage.getItem("storeStorage")) || [];
-    const storeDisplayHorizontal = document.querySelector('#stores-display-horizontal');
+    const storeDisplayHorizontal = document.getElementById('stores-display-horizontal');
+    const storeDisplayVertical = document.getElementById('stores-display-vertical');
     storeDisplayHorizontal.innerHTML = "";
+    storeDisplayVertical.innerHTML = "";
 
-    stores.forEach(function (store){
-        const newHorizontalCard = document.createElement("div");
-        newHorizontalCard.classList.add("horizontal-card");
+    let filteredStores;
+    if (selectedLocation === "all") {
+        filteredStores = stores;
+    } else {
+        filteredStores = stores.filter(store => store.storeLocation === selectedLocation);
+    }
 
-        newHorizontalCard.innerHTML = `
+    filteredStores.forEach(function (store) {
+        const newCard = document.createElement("div");
+        newCard.classList.add("horizontal-card");
+        const newVerticalCard = document.createElement("div");
+        newVerticalCard.classList.add("vertical-card");
+
+        newCard.innerHTML = `
             <div class="store-img">
-                <img src='./assets/img/aswa9-salam.jpg'> <img/>
+                <img src="./assets/img/aswa9-salam.jpg">
             </div>
             <div class="store-detail-holder">
-            <h3> ${store.storeName} </h3>
-            <p> <i class="fas fa-map"></i> ${store.storeLocation}</p>
+                <h3> ${store.storeName} </h3>
+                <p> ${store.storeLocation}</p>
             </div>
-        `
-      storeDisplayHorizontal.appendChild(newHorizontalCard)
-    })
+        `;
+
+        newVerticalCard.innerHTML = `
+            <div class="store-img">
+                <img src="./assets/img/aswa9-salam.jpg">
+            </div>
+            <div class="store-detail-holder">
+                <h3> ${store.storeName} </h3>
+                <p> ${store.storeLocation}</p>
+            </div>
+        `;
+
+        storeDisplayHorizontal.appendChild(newCard);
+        storeDisplayVertical.appendChild(newVerticalCard);
+    });
 }
+
+function toggleDisplay(displayMode) {
+    const verticalDisplay = document.getElementById('stores-display-vertical');
+    const horizontalDisplay = document.getElementById('stores-display-horizontal');
+    if (displayMode === "vertical") {
+        verticalDisplay.style.display = "flex";
+        horizontalDisplay.style.display = "none";
+    } else {
+        verticalDisplay.style.display = "none";
+        horizontalDisplay.style.display = "flex";
+    }
+}
+
+document.getElementById("btn").addEventListener("click", () => {
+    toggleDisplay("vertical");
+});
+
+document.getElementById("btn1").addEventListener("click", () => {
+    toggleDisplay("horizontal");
+});
